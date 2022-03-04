0d56feed0d8927f484f4971fb18350640e508a11
AndradaCincu <73743977+AndradaCincu@users.noreply.github.com>
2022-03-04 13:25:30 +0200

    Delete DEBIAN directory

--- DEBIAN/createDeb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp ../asteroides.py games-in-pygame/usr/local/games
-cp asteroides    games-in-pygame/usr/local/games
-chmod 755  games-in-pygame/usr/local/games/asteroides
-dpkg-deb --build games-in-pygame
